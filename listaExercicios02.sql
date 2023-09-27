47cd3a8f214d7520f1cd573b93ba9af2de46b6a4
Pandamaximus28409 <102782209+Pandamaximus28409@users.noreply.github.com>
2023-09-26 21:25:19 -0300

    10
    10

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,28 +1,7 @@
-Esta stored procedure adiciona um novo livro à tabela Livro com tratamento de erros.
-
-Parâmetros de entrada:
- titulo_livro
-editora_id
-ano_publicacao
-numero_paginas
-categoria_id
-
-Parâmetro de saída:
-mensagem: Mensagem de erro ou sucesso depois da execução.
-
-CREATE PROCEDURE sp_AdicionarLivro(IN titulo_livro VARCHAR(255), IN editora_id INT, IN ano_publicacao INT, IN numero_paginas INT, IN categoria_id INT, OUT mensagem VARCHAR(255))
+CREATE PROCEDURE sp_LivrosESeusAutores()
 BEGIN
-Verifica se já existe um livro com o mesmo título.
-Se existir, retorna uma mensagem de erro.
-    DECLARE EXIT HANDLER FOR 1062
-    BEGIN
-        SET mensagem = 'Erro: Já existe um livro com esse título.';
-    END;
-
-Insere o novo livro na tabela Livro.
-    INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
-    VALUES (titulo_livro, editora_id, ano_publicacao, numero_paginas, categoria_id);
-
-Define uma mensagem de sucesso após a inserção.
-    SET mensagem = 'Livro adicionado com sucesso.';
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS Autor
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
 END;
